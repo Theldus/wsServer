ac7d12e043db7f9586beea5c8fd15b4cffbe31b5
Theldus <davidsondfgl@gmail.com>
2020-11-02 12:12:19 -0300

    Enhancement: Bumping dates

--- example/send_receive.c
+++ example/send_receive.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2019 Davidson Francis <davidsondfgl@gmail.com>
+ * Copyright (C) 2016-2020 Davidson Francis <davidsondfgl@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/handshake/handshake.c
+++ src/handshake/handshake.c
@@ -1,19 +1,19 @@
 /*
-Copyright (C) 2016  Davidson Francis <davidsondfgl@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
+ * Copyright (C) 2016-2020  Davidson Francis <davidsondfgl@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 #include <sha1.h>
 #include <base64.h>
 #include <ws.h>
@@ -91,7 +91,7 @@ int get_handshake_response(char *hsrequest, char **hsresponse)
 
 	s = strtok(s,    " ");
 	s = strtok(NULL, " ");
-	
+
 	ret = get_handshake_accept(s, &accept);
 	if (ret < 0)
 		return (ret);
