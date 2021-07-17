3639d55d895ac294b2c58f44a458fe2938b2a5c4
Davidson Francis <davidsondfgl@gmail.com>
2021-07-17 03:30:15 -0300

    Enhancement: Output validation no longer needs 'not passed' list

--- tests/validate_output.py
+++ tests/validate_output.py
@@ -32,48 +32,6 @@ ret = 0
 # Target file
 file = "wsserver_autobahn/report/index.json"
 
-#
-# 'NOT PASSED' list
-#
-# Here we implement them as a dictionary as the lookup time
-# is so much better than an ordinary 'list'.
-#
-# Although they seem like a lot of errors, they are all related to 'invalid
-# UTF-8 sequences validation'. Since wsServer does not validate UTF-8 sequences,
-# it is natural that all tests related to this do not pass.
-#
-# However, these tests represent about ~25% (76 out of 298) of the total tests,
-# and wsServer passes everything else. Note, therefore, that wsServer is also
-# fully capable of sending and receiving UTF-8 strings, it just doesn't validate
-# them.
-#
-# More info at:
-#   https://github.com/Theldus/wsServer/blob/master/doc/AUTOBAHN.md
-#
-np_dict = {
-	"6.3.1": 1,  "6.3.2": 1,
-	"6.4.1": 1,  "6.4.2": 1, "6.4.3": 1, "6.4.4": 1,
-	"6.6.1": 1,  "6.6.3": 1, "6.6.4": 1, "6.6.6": 1, "6.6.8": 1, "6.6.10": 1,
-	"6.8.1": 1,  "6.8.2": 1,
-	"6.10.1": 1, "6.10.2": 1, "6.10.3": 1,
-	"6.11.5": 1,
-	"6.12.1": 1, "6.12.2": 1, "6.12.3": 1, "6.12.4": 1, "6.12.5": 1, "6.12.6": 1,
-	"6.12.7": 1, "6.12.8": 1,
-	"6.13.1": 1, "6.13.2": 1, "6.13.3": 1, "6.13.4": 1, "6.13.5": 1,
-	"6.14.1": 1, "6.14.2": 1, "6.14.3": 1, "6.14.4": 1, "6.14.5": 1, "6.14.6": 1,
-	"6.14.7": 1, "6.14.8": 1, "6.14.9": 1, "6.14.10": 1,
-	"6.15.1": 1,
-	"6.16.1": 1, "6.16.2": 1, "6.16.3": 1,
-	"6.17.1": 1, "6.17.2": 1, "6.17.3": 1, "6.17.4": 1, "6.17.5": 1,
-	"6.18.1": 1, "6.18.2": 1, "6.18.3": 1, "6.18.4": 1, "6.18.5": 1,
-	"6.19.1": 1, "6.19.2": 1, "6.19.3": 1, "6.19.4": 1, "6.19.5": 1,
-	"6.20.1": 1, "6.20.2": 1, "6.20.3": 1, "6.20.4": 1, "6.20.5": 1, "6.20.6": 1,
-	"6.20.7": 1,
-	"6.21.1": 1, "6.21.2": 1, "6.21.3": 1, "6.21.4": 1, "6.21.5": 1, "6.21.6": 1,
-	"6.21.7": 1, "6.21.8": 1,
-	"7.5.1": 1
-}
-
 def check_results():
 	ret = 0
 
@@ -97,9 +55,8 @@ def check_results():
 		# We must known that failure, otherwise, we're not passing the tests
 		# and we must inform the user.
 		if status == "FAILED":
-			if not test in np_dict:
-				sys.stderr.write("Test {} was not expected to fail!\n".format(test))
-				ret = 1
+			sys.stderr.write("Test {} was not expected to fail!\n".format(test))
+			ret = 1
 	return ret
 
 print("[+] Checking output...")
